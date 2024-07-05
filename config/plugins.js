a2a9f67154a0a5890226b8925409ed13b7c74f9f
HarshilKhimasia <harshilkhimasia22@gmail.com>
2024-07-05 09:49:12 +0530

    Configured Wasabi upload provider

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,15 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  upload: {
+    provider: "aws-s3",
+    providerOptions: {
+      accessKeyId: env("WASABI_ACCESS_KEY_ID"),
+      secretAccessKey: env("WASABI_ACCESS_SECRET"),
+      region: env("WASABI_REGION"),
+      endpoint: env("WASABI_ENDPOINT"), // This is important for Wasabi
+      s3ForcePathStyle: true, // Required for Wasabi
+      params: {
+        Bucket: env("WASABI_BUCKET"),
+      },
+    },
+  },
+});
