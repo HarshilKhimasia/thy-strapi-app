c4674eca7cab813f13f53cda5657118f82a3eaa0
HarshilKhimasia <harshilkhimasia22@gmail.com>
2024-07-07 12:35:34 +0530

    config plugin.js upadted in both

--- config/plugins.js
+++ config/plugins.js
@@ -1,15 +1,15 @@
 module.exports = ({ env }) => ({
   upload: {
-    provider: "wasabi",
+    provider: "aws-s3",
     providerOptions: {
-      region: env("WASABI_REGION"),
       accessKeyId: env("WASABI_ACCESS_KEY_ID"),
       secretAccessKey: env("WASABI_SECRET_ACCESS_KEY"),
-      bucket: env("WASABI_BUCKET_NAME"),
-      cdnUrl: env("WASABI_CDN_URL"),
+      region: env("WASABI_REGION"),
       params: {
-        ACL: "private", // Optional: Set ACL for uploaded objects
+        Bucket: env("WASABI_BUCKET_NAME"),
       },
+      baseUrl: env("WASABI_CDN_URL"), // Optional: if you're using a CDN
+      basePath: "/", // Optional: base path for all uploads
     },
   },
 });
