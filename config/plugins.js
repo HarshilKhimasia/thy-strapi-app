e10b7bd325a3a3cb4398455b00b05003faf8fff8
HarshilKhimasia <harshilkhimasia22@gmail.com>
2024-07-08 08:24:55 +0530

    config plugin.js upadted in both

--- config/plugins.js
+++ config/plugins.js
@@ -1,31 +1,18 @@
 module.exports = ({ env }) => ({
-  // ...
   upload: {
     config: {
-      provider: "aws-s3",
+      provider: "@strapi/provider-upload-aws-s3",
       providerOptions: {
-        baseUrl: env("CDN_URL"),
-        rootPath: env("CDN_ROOT_PATH"),
-        s3Options: {
-          credentials: {
-            accessKeyId: env("AWS_ACCESS_KEY_ID"),
-            secretAccessKey: env("AWS_ACCESS_SECRET"),
-          },
-          region: env("AWS_REGION"),
-          params: {
-            ACL: "private",
-            signedUrlExpires: env("AWS_SIGNED_URL_EXPIRES", 15 * 60),
-            Bucket: env("AWS_BUCKET"),
-          },
-          endpoint: env("WASABI_ENDPOINT"),
+        accessKeyId: env("WASABI_ACCESS_KEY_ID"),
+        secretAccessKey: env("WASABI_SECRET_ACCESS_KEY"),
+        region: env("WASABI_REGION"),
+        endpoint: "https://s3.eu-west-1.wasabisys.com", // Wasabi's S3 API endpoint
+        params: {
+          Bucket: env("WASABI_BUCKET_NAME"),
         },
-      },
-      actionOptions: {
-        upload: {},
-        uploadStream: {},
-        delete: {},
+        baseUrl: env("WASABI_CDN_URL"),
+        basePath: "",
       },
     },
   },
-  // ...
 });
