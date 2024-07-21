1f7ee50b23e834d2122513ccd50c4f688a8fb47f
HarshilKhimasia <harshilkhimasia22@gmail.com>
2024-07-21 17:18:44 +0530

    url of fastly and wasabi changed to thy-properties with changes to config and variables

--- config/env/production/middlewares.js
+++ config/env/production/middlewares.js
@@ -13,16 +13,16 @@ module.exports = [
             "data:",
             "blob:",
             "market-assets.strapi.io",
-            "thy-test-bucket2.s3.eu-west-1.wasabisys.com", // Update with your Wasabi bucket URL
-            "thy-test.global.ssl.fastly.net", // Fastly CDN URL
+            "thy-properties.s3.eu-west-1.wasabisys.com", // Update with your Wasabi bucket URL
+            "thy-properties.global.ssl.fastly.net", // Fastly CDN URL
           ],
           "media-src": [
             "'self'",
             "data:",
             "blob:",
             "market-assets.strapi.io",
-            "thy-test-bucket2.s3.eu-west-1.wasabisys.com", // Update with your Wasabi bucket URL
-            "thy-test.global.ssl.fastly.net", // Fastly CDN URL
+            "thy-properties.s3.eu-west-1.wasabisys.com", // Update with your Wasabi bucket URL
+            "thy-properties.global.ssl.fastly.net", // Fastly CDN URL
           ],
           upgradeInsecureRequests: null,
         },
